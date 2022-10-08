a5ecd41acbfaa7111204b7c0f4b43fba21850aec
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-10-08 13:01:12 +0100

    test

--- primes.py
+++ primes.py
@@ -1,6 +1,39 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
+
+from tabnanny import check
 def primes(number_of_primes):
+
     list = []
+
+    i = 1
+
+    currentNumber = 2
+
+    list.append(2)
+
+    while i <= number_of_primes:
+        currentNumber = currentNumber + 1
+        x = 2
+        while x < currentNumber:
+            if x == currentNumber:
+                list.append(currentNumber)
+            elif currentNumber % x:
+                break
+
+        i = i + 1
+
+    print(list)
     return list
+
+
+# while True:
+#     try:
+#         primes(int(input().split("")))
+#     except ValueError:
+#         print("Some input could not be converted to a number!")
+#     else:
+#         break
